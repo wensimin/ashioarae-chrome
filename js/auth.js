bc3ead452b01f3498387e9cd92792a86ec95a8f1
wensimin <hukaizhiyu2@gmail.com>
2020-08-23 14:34:16 +0800

    认证页面bug fix

--- js/auth.js
+++ js/auth.js
@@ -8,9 +8,22 @@ $(function () {
         let password = $("#password").val();
         let config = {"username": username, "password": password};
         chrome.storage.sync.set(config);
-        $.get(host + "user", {}, function (result) {
+        $.ajax(host + "user", {
+            headers: {
+                "Authorization": "Basic " + btoa(config["username"] + ":" + config["password"])
+            },
+            method: "get"
+        }).done(result => {
             if (result === username) {
-                alert("认证成功");
+                alert("认证成功,刷新页面");
+            } else {
+                alert("认证失败,检查用户名密码")
+            }
+        }).fail(r => {
+            if (r.status === 401) {
+                alert("认证失败,检查用户名密码")
+            } else {
+                alert("未知错误")
             }
         });
     });
